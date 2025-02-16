91b3d78f4574003167461e03c963ff3679d29997
YAKOOzA <boogeyyakooza@gmail.com>
2025-02-17 01:00:52 +0330

    add Discount model and update product pricing logic based on category discounts

--- home/models.py
+++ home/models.py
@@ -4,21 +4,29 @@ from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 from django.db.models.fields import CharField
 from django.template.defaultfilters import title
+from django.utils import timezone
 from django.utils.text import slugify
 from slugify import slugify as persian_slugify
 from datetime import datetime
 from django.db.models import JSONField
 from shop import settings
+from django.db.models.signals import post_save, m2m_changed
+from django.dispatch import receiver
+
+
 # Create your models here.
 
 
 class Info(models.Model):
     site_name = models.CharField(max_length=100)
     support_number = models.CharField(max_length=100)
+
     def __str__(self):
         return self.site_name
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
+
 
 # class Users(AbstractBaseUser, PermissionsMixin):
 #     username = models.CharField(max_length=100)
@@ -51,37 +59,72 @@ class Info(models.Model):
 #     home_phone = models.CharField(max_length=100)
 #     payment_info = JSONField(blank=True, default=list, help_text="Store detailed payment information as JSON", editable=False)
 
-    # def __str__(self):
-    #     return self.user.phone  # Use the related user's phone number
-    #
-    # def save(self, *args, **kwargs):
-    #     # Ensure the payment_info is a list if it's empty
-    #     if not self.payment_info:
-    #         self.payment_info = []
-    #     super().save(*args, **kwargs)
+# def __str__(self):
+#     return self.user.phone  # Use the related user's phone number
+#
+# def save(self, *args, **kwargs):
+#     # Ensure the payment_info is a list if it's empty
+#     if not self.payment_info:
+#         self.payment_info = []
+#     super().save(*args, **kwargs)
+
+
+class Discount(models.Model):
+    title = models.CharField(max_length=100, verbose_name='عنوان', db_index=True)
+    percentage = models.PositiveIntegerField(verbose_name="درصد تخفیف")
+    start_date = models.DateTimeField(null=True, blank=True, verbose_name='تاریخ شروع')
+    end_date = models.DateTimeField(null=True, blank=True, verbose_name='تاریخ پایان')
+    is_active = models.BooleanField(default=False, verbose_name='فعال / غیر فعال')
+
+    class Meta:
+        verbose_name = 'تخفیف'
+        verbose_name_plural = 'تخفیف ها'
+
+    def is_valid(self):
+        now = timezone.now()
+        start_ok = self.start_date is None or self.start_date <= now
+        end_ok = self.end_date is None or self.end_date >= now
+        return self.is_active and start_ok and end_ok
+
+    def __str__(self):
+        return self.title
+
 
 class Categories(models.Model):
     name = models.CharField(max_length=100, verbose_name='عنوان', db_index=True)
     url_title = models.CharField(max_length=100, null=True, verbose_name='عنوان در url', db_index=True)
     content = models.TextField(null=True, blank=True, verbose_name='توضیحات')
+    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="تخفیف")
     is_active = models.BooleanField(default=False, verbose_name='فعال / غیر فعال')
 
     class Meta:
         verbose_name = 'سته بندی'
         verbose_name_plural = 'دسته بندی ها'
+
     def __str__(self):
         return self.name
 
+
+@receiver(post_save, sender=Categories)
+def update_product_prices(sender, instance, created, **kwargs):
+    if hasattr(instance, 'discount') and instance.discount:  #اگر دسته بندی تخفیف داشت
+        if instance.discount.is_valid():  #اگر تخفیف معتبر بود
+            products = Products.objects.filter(category=instance)  #محصولات مرتبط با این دسته بندی را پیدا کن
+            for product in products:
+                product.save()  #محصولات رو save کن تا قیمت بعد از تخفیف به روز شود
+
+
 class ProductTag(models.Model):
     title = models.CharField(max_length=100, verbose_name='عنوان تگ', db_index=True)
-    description = models.CharField(null=True, blank= True, verbose_name='توضیحات')
+    description = models.CharField(null=True, blank=True, verbose_name='توضیحات')
 
     class Meta:
         verbose_name = 'تگ محصول'
-        verbose_name_plural = 'تگ ها' #tag ----> "tags"
+        verbose_name_plural = 'تگ ها'  #tag ----> "tags"
 
     def __str__(self):
         return self.title
+
 
 class ProductPublisher(models.Model):
     title = models.CharField(max_length=100, verbose_name='نام انتشارات', db_index=True)
@@ -94,6 +137,7 @@ class ProductPublisher(models.Model):
     def __str__(self):
         return self.title
 
+
 class ProductsInfo(models.Model):
     seller_name = models.CharField(max_length=100, default="کتابخانه", blank=True, null=True, verbose_name='فروشنده')
     writer = models.CharField(max_length=100, default=None, verbose_name='نویسنده')
@@ -101,7 +145,8 @@ class ProductsInfo(models.Model):
     print = models.CharField(max_length=100, default=None, verbose_name='نوبت چاپ')
     translator = models.CharField(max_length=100, blank=True, null=True, verbose_name='مترجم')
     pages = models.IntegerField(default=0, verbose_name='تعداد صفحات')
-    language = models.CharField(max_length=100, choices=[("en", "English"), ("fa", "Farsi"), ("ar", "Arabic")], default='fa', verbose_name='زبان')
+    language = models.CharField(max_length=100, choices=[("en", "English"), ("fa", "Farsi"), ("ar", "Arabic")],
+                                default='fa', verbose_name='زبان')
 
     def __str__(self):
         return f'{self.seller_name} - {self.writer}'
@@ -110,21 +155,25 @@ class ProductsInfo(models.Model):
         verbose_name = 'اطلاعات محصول'
         verbose_name_plural = 'اطلاعات محصولات'
 
+
 class Products(models.Model):
     title = models.CharField(max_length=100, verbose_name='عنوان', db_index=True)
     price = models.IntegerField(default=0, help_text='تومان', verbose_name='قیمت', db_index=True)
-    price_after_discount = models.IntegerField(default=None, blank=True, null=True, verbose_name='قیمت بعد از تخفیف')
+    discounted_price = models.IntegerField(null=True, blank=True, default=0, help_text='تومان',
+                                           verbose_name='قیمت پس از تخفیف', db_index=True)
     content = models.TextField(null=True, verbose_name='توضیحات')
-    Info = models.OneToOneField(ProductsInfo, on_delete=models.CASCADE, blank=True, null=True, related_name='Product_Information', verbose_name='اطلاعات تکمیلی')
-    category = models.ManyToManyField(Categories, blank=True,verbose_name='دسته بندی')
+    Info = models.OneToOneField(ProductsInfo, on_delete=models.CASCADE, blank=True, null=True,
+                                related_name='Product_Information', verbose_name='اطلاعات تکمیلی')
+    category = models.ManyToManyField(Categories, blank=True, verbose_name='دسته بندی')
     quantity = models.PositiveIntegerField(default=0, verbose_name='تعداد')
-    slug = models.SlugField(max_length=100, unique=True, db_index=True, blank=True, null=True, verbose_name='عنوان در url')
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, blank=True, null=True,
+                            verbose_name='عنوان در url')
     # rate = models.IntegerField(default=0, editable=False, verbose_name='امتیاز')
     sell_count = models.IntegerField(default=0, verbose_name='تعداد فروش')
     # test = models.CharField(null=True, blank=True)
     is_active = models.BooleanField(default=False, verbose_name='فعال / غیر فعال')
-    last_update = models.DateTimeField(auto_now=True, verbose_name='آخرین تغییرات', null=True) # problem:
-    image = models.ImageField(upload_to='images/' ,blank=True, null=True, verbose_name='تصویر', default='default.png')
+    last_update = models.DateTimeField(auto_now=True, verbose_name='آخرین تغییرات', null=True)  # problem:
+    image = models.ImageField(upload_to='images/', blank=True, null=True, verbose_name='تصویر', default='default.png')
     # image = models.CharField(max_length=200, blank=True, verbose_name='آدرس تصویر')
     is_deleted = models.BooleanField(default=False, verbose_name='حذف شده / نشده')
     tags = models.ManyToManyField(ProductTag, blank=True, verbose_name='تگ', default=None)
@@ -132,21 +181,29 @@ class Products(models.Model):
     class Meta:
         verbose_name = 'محصول'
         verbose_name_plural = 'محصولات'
+
     def __str__(self):
         return self.title
-    def save(self, *args, **kwargs):
 
-        # if self.image:
-        #     if self.title:
-        #         if self.title.isascii():  # Check if title is in English
-        #             slugified_title = slugify(self.title)
-        #         else:
-        #             slugified_title = persian_slugify(self.title)
-        #         self.image_path = f'images/{slugified_title}/{self.image.name}'
-        #     else:
-        #         self.image_path = f'images/{self.image.name}'
-        # self.slug = slugify(self.title)
+    def save(self, *args, **kwargs):
+        valid_discounts = []
+
+        for category in self.category.all():
+            if hasattr(category, 'discount') and category.discount:
+                if category.discount.is_valid():
+                    valid_discounts.append(category.discount.percentage)
+
+        max_discount = max(valid_discounts) if valid_discounts else 0
+
+        if max_discount:
+            self.discounted_price = self.price * (100 - max_discount) // 100
+        else:
+            self.discounted_price = None
+
         super().save(*args, **kwargs)
 
 
-
+@receiver(m2m_changed, sender=Products.category.through)
+def update_products_on_category_change(sender, instance, action, **kwargs):
+    if action in ("post_add", "post_remove", "post_clear"):
+        instance.save()
