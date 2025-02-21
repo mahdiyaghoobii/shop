7ac1bf67c37f98e9254e509b0d544ac8320d7733
YAKOOzA <boogeyyakooza@gmail.com>
2025-02-21 14:58:07 +0330

    extend refresh token lifetime to 7 days

--- shop/settings.py
+++ shop/settings.py
@@ -146,7 +146,7 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
