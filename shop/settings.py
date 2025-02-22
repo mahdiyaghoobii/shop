23d5963b1e675663917af7bb5bd6dc2ea8cca7a0
YAKOOzA <boogeyyakooza@gmail.com>
2025-02-22 16:52:26 +0330

    ...

--- shop/settings.py
+++ shop/settings.py
@@ -21,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY ='django-insecure-p%t)&gis%iiow347va@dl@g4kwq%qf+v9f1zotc1sw*e!6lfr1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG =True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '884e-2a12-5940-f25a-00-2.ngrok-free.app'
+]
 
 # Application definition
 
@@ -86,9 +90,9 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'NAME': 'eshopdb',
+        'USER': 'eshop',
+        'PASSWORD': 'mahdi',
         'HOST': 'localhost',
         'PORT': '5432',
     }}
