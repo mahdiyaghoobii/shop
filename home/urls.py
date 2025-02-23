39ef715f340c7209bba50cda4bd1af182e593257
YAKOOzA <boogeyyakooza@gmail.com>
2025-02-23 16:23:47 +0330

    Add clear_basket and add_to_basket routes to URLs

--- home/urls.py
+++ home/urls.py
@@ -5,10 +5,12 @@ from . import views
 
 urlpatterns = [
     # path('best_product/', views.BestProductSlider.as_view(), name='best_product'),
+    path('clear_basket/', views.clear_basket.as_view(), name='clear_basket'),
     path('list-product/', views.productlist.as_view(), name='product-list'),
     path('most-sells-products/', views.product_most_sells.as_view(), name='most-sells-products'),
     path('filter/', views.ProductFilter.as_view(), name='my_view'),
     path('slider/', views.Slides.as_view(), name='slider'),
     path('<slug:slug>/', views.Product_detail.as_view(), name='product_detail'),
+    path('add-to-basket/<slug:slug>/', views.add_basket.as_view(), name='add_to_basket'),
 
 ]
