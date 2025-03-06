03d7948ea31011c1a22da84abf374f59ad79974e
YAKOOzA <boogeyyakooza@gmail.com>
2025-03-06 21:27:40 +0330

    add set-rate function and popular-product API url path

--- home/urls.py
+++ home/urls.py
@@ -8,11 +8,12 @@ urlpatterns = [
     path('clear_basket/', views.clear_basket.as_view(), name='clear_basket'),
     path('list-product/', views.productlist.as_view(), name='product-list'),
     path('most-sells-products/', views.product_most_sells.as_view(), name='most-sells-products'),
+    path('popular-products/', views.popular_product.as_view(), name='popular-products'),
+    path('setrate/', views.rating.as_view(), name='rating'),
     path('filter/', views.ProductFilter.as_view(), name='my_view'),
     path('slider/', views.Slides.as_view(), name='slider'),
     # path('<int:pk>/', views.Product_detail.as_view(), name='product_detail'),
     re_path(r'(?P<slug>[\w\-_۰-۹آ-ی]+)/', views.Product_detail.as_view(), name='product_detail'),
-
     path('add-to-basket/<slug:slug>/', views.add_basket.as_view(), name='add_to_basket'),
 
 ]
