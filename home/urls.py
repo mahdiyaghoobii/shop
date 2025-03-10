014e28d423ab2f98450d45633645094c3cd97f59
YAKOOzA <boogeyyakooza@gmail.com>
2025-03-10 14:56:06 +0330

    renamed

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     # path('best_product/', views.BestProductSlider.as_view(), name='best_product'),
     path('clear_basket/', views.clear_basket.as_view(), name='clear_basket'),
-    path('list-product/', views.productlist.as_view(), name='product-list'),
+    path('list-product/', views.ProductList.as_view(), name='product-list'),
     path('most-sells-products/', views.product_most_sells.as_view(), name='most-sells-products'),
     path('popular-products/', views.popular_product.as_view(), name='popular-products'),
     path('setrate/', views.rating.as_view(), name='rating'),
