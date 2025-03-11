46bd15ccc186af465f9ef8456245ee1924971118
YAKOOzA <boogeyyakooza@gmail.com>
2025-03-11 12:20:18 +0330

    ...

--- main/views.py
+++ main/views.py
@@ -9,24 +9,6 @@ from home.models import Products
 
 def index(request):
     return render(request, 'main/index_page.html')
-
-def product_list(request):
-    products = Products.objects.all().order_by('price')[:15]# :5 = last 15 # '-price' so'oodi
-    # number_of_products = products.count()
-    return render(request, 'main/product_list.html', {
-        # 'number_of_products': number_of_products,
-        'product': products
-    })
-
-
-
-
-def product_detail(request, slug):
-    product = get_object_or_404(Products, slug=slug)
-    return render(request, 'main/product-details.html', {
-        'product': product
-    })
-
 def contact_page (request):
     return render(request, 'main/contact_page.html')
 
